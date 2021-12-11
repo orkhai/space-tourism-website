585bba8012b5d64f02394b551f07c0c3498f0243
Orkhai Dickson <dicksonorkhai@gmail.com>
2021-12-11 18:33:27 +0100

    Rendering home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Fragment } from "react";
-// import Home from "./components/Home";
-import Destination from "./components/Destination";
+import Home from "./components/Home";
+// import Destination from "./components/Destination";
 // import Crew from "./components/Crew";
 // import Technology from "./components/Technology";
 
 function App() {
   return (
     <Fragment>
-      {/* <Home /> */}
-      <Destination />
+      <Home />
+      {/* <Destination /> */}
       {/* <Crew /> */}
       {/* <Technology /> */}
     </Fragment>
